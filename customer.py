cb4e9253e2b015d89bae0753d256a16176a09760
ppoojariconfluent <128455475+ppoojariconfluent@users.noreply.github.com>
2023-08-28 17:46:43 +0530

    Add files via upload

---
+++ customer.py
@@ -0,0 +1,178 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+# Copyright 2020 Confluent Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+
+# A simple example demonstrating use of AvroSerializer.
+
+import argparse
+import os
+from uuid import uuid4
+import uuid
+import random
+
+from six.moves import input
+
+from confluent_kafka import Producer
+from confluent_kafka.serialization import StringSerializer, SerializationContext, MessageField
+from confluent_kafka.schema_registry import SchemaRegistryClient
+from confluent_kafka.schema_registry.avro import AvroSerializer
+
+
+class Customer(object):
+    """
+    User record
+
+    Args:
+        name (str): User's name
+
+        favorite_number (int): User's favorite number
+
+        favorite_color (str): User's favorite color
+
+        address(str): User's address; confidential
+    """
+
+    def __init__(self, name, email, phone, account_number):
+        self.name = name
+        self.account_number = account_number
+        self.email = email
+        # address should not be serialized, see user_to_dict()
+        self.phone = phone
+
+
+def customer_to_dict(customer, ctx):
+    """
+    Returns a dict representation of a User instance for serialization.
+
+    Args:
+        user (User): User instance.
+
+        ctx (SerializationContext): Metadata pertaining to the serialization
+            operation.
+
+    Returns:
+        dict: Dict populated with user attributes to be serialized.
+    """
+
+    # User._address must not be serialized; omit from dict
+    return dict(name=customer.name,
+                account_number=customer.account_number,
+                email=customer.email,
+                phone=customer.phone
+                )
+
+
+def delivery_report(err, msg):
+    """
+    Reports the failure or success of a message delivery.
+
+    Args:
+        err (KafkaError): The error that occurred on None on success.
+
+        msg (Message): The message that was produced or failed.
+
+    Note:
+        In the delivery report callback the Message.key() and Message.value()
+        will be the binary format as encoded by any configured Serializers and
+        not the same object that was passed to produce().
+        If you wish to pass the original object(s) for key and value to delivery
+        report callback we recommend a bound callback or lambda where you pass
+        the objects along.
+    """
+
+    if err is not None:
+        print("Delivery failed for User record {}: {}".format(msg.key(), err))
+        return
+    print('User record {} successfully produced to {} [{}] at offset {}'.format(
+        msg.key(), msg.topic(), msg.partition(), msg.offset()))
+
+
+def main(args):
+    topic = 'customer-details'
+    schema = "customer.avsc"
+
+
+    path = os.path.realpath(os.path.dirname(__file__))
+    with open(f"{path}/avro/{schema}") as f:
+        schema_str = f.read()
+
+    schema_registry_conf = {
+        'url': 'https://psrc-9wjxm.southeastasia.azure.confluent.cloud',
+        'basic.auth.user.info': 'RFMIVEFVBUNOOZ7P:lN0i3dE24QS9zk1BAHGJdV14T+12Lee1hTtXDQbD6KmIBgGZCbm1OI4yLF5GcGDD'
+        }
+    schema_registry_client = SchemaRegistryClient(schema_registry_conf)
+
+    avro_serializer = AvroSerializer(schema_registry_client,
+                                     schema_str,
+                                     customer_to_dict)
+
+    string_serializer = StringSerializer('utf_8')
+
+    producer_conf = {'bootstrap.servers': 'pkc-p11xm.us-east-1.aws.confluent.cloud:9092',
+    'sasl.mechanisms': 'PLAIN',
+    'security.protocol': 'SASL_SSL',
+    'sasl.username': '5WURIXNIMRZUTSID',
+    'sasl.password': 'Fn5rBC4YNMw3HCAxv+TS2yxvfEBmcZEw9Hjl/DN3n3IrWHMR/Ro3JzAIvNFfz1dU',
+    }
+
+    producer = Producer(producer_conf)
+
+    print("Producing transactional records to topic {}. ^C to exit.".format(topic))
+    for i in range(30):
+        # Serve on_delivery callbacks from previous calls to produce()
+        producer.poll(0.0)
+        try:
+            # transaction_transaction_id = input("Enter transaction_id: ")
+            # transaction_account_number = input("Enter account number: ")
+            # transaction_amount = input("Enter amount: ")
+            # transaction_transaction_type = input("Enter transaction type: ")
+            
+            customer_account_number=f"account_{i}"
+            customer_name=f"Customer {i}"
+            customer_email=f"customer{i}@bank.com"
+            customer_phone=f"+123456789{i}"
+            customer = Customer(name=customer_name,
+                        account_number=customer_account_number,
+                        email=customer_email,
+                        phone=customer_phone)
+            producer.produce(topic=topic,
+                             key=string_serializer(str(uuid4())),
+                             value=avro_serializer(customer, SerializationContext(topic, MessageField.VALUE)),
+                             on_delivery=delivery_report)
+        except KeyboardInterrupt:
+            break
+        except ValueError:
+            print("Invalid input, discarding record...")
+            continue
+
+    print("\nFlushing records...")
+    producer.flush()
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description="AvroSerializer example")
+    parser.add_argument('-b', dest="bootstrap_servers", required=True,
+                        help="Bootstrap broker(s) (host[:port])")
+    parser.add_argument('-s', dest="schema_registry", required=True,
+                        help="Schema Registry (http(s)://host[:port]")
+    parser.add_argument('-t', dest="topic", default="example_serde_avro",
+                        help="Topic name")
+    parser.add_argument('-p', dest="specific", default="true",
+                        help="Avro specific record")
+
+    main(parser.parse_args())
+
